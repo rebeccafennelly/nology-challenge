236d91ec55be659b7c65e968c3c13d23cfac0714
rebeccafennelly <64370782+rebeccafennelly@users.noreply.github.com>
2020-05-15 17:05:16 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// const firstname;
+// const lastname;
+// function getFirstName () {
+//     return document.getElementById
+// }
+
+
+
+
+
+const fname = document.getElementById('firstname').value;
+
+const lname = document.getElementById('lastname').value;
+
+const message = document.getElementById('mainbox').value;
+
+
+const display = () => {
+
+alert (`Thank you ${firstname.value} ${lastname.value}. I will get back to you as soon as I can. Stay safe! Love Bex X`);
+}
